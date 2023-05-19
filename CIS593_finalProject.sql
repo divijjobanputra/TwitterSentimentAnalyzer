90e60859548f4fdbd817d6a4e92f049ceac654b2
The Divij Jobanputra <divijjobanputra@gmail.com>
2023-05-18 23:01:11 -0400

    Add files via upload

---
+++ CIS593_finalProject.sql
@@ -0,0 +1,10 @@
+Select * from Twitter_Sentiment_Analysis;
+
+Select * from Twitter_Sentiment_Analysis 
+where analysis = 'Positive';
+
+Select * from Twitter_Sentiment_Analysis 
+where analysis = 'Negative';
+
+Select * from Twitter_Sentiment_Analysis 
+where analysis = 'Neutral';
